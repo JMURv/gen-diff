fae421b95082a6eabe150c70067a218d0b142462
JMURv <architect.lock@outlook.com>
2022-10-01 14:20:51 +0300

    More test's added

--- gendiff/tests/test_main.py
+++ gendiff/tests/test_main.py
@@ -1,77 +1,104 @@
 from gendiff import generate_diff
+import pytest
 
-json_test = generate_diff(
-    'gendiff/tests/fixtures/plane/json/file1.json',
-    'gendiff/tests/fixtures/plane/json/file2.json'
+
+@pytest.mark.parametrize(
+    ('test_1', 'test_2', 'expected'),
+    [
+        pytest.param(
+            'gendiff/tests/fixtures/plane/json/file1.json',
+            'gendiff/tests/fixtures/plane/json/file2.json',
+            'gendiff/tests/fixtures/plane/first_test.txt'
+        ),
+        pytest.param(
+            'gendiff/tests/fixtures/plane/yml/file1.yml',
+            'gendiff/tests/fixtures/plane/yml/file2.yml',
+            'gendiff/tests/fixtures/plane/first_test.txt'
+        ),
+    ]
 )
-yml_test = generate_diff(
-    'gendiff/tests/fixtures/plane/yml/file1.yml',
-    'gendiff/tests/fixtures/plane/yml/file2.yml'
+def test_flat(test_1, test_2, expected):
+    with open(expected, 'r') as f:
+        result = f.read()
+    assert generate_diff(test_1, test_2) == result
+
+
+@pytest.mark.parametrize(
+    ('test_1', 'test_2', 'expected'),
+    [
+        pytest.param(
+            'gendiff/tests/fixtures/nested/json/file1.json',
+            'gendiff/tests/fixtures/nested/json/file2.json',
+            'gendiff/tests/fixtures/nested/nested_result'
+        ),
+        pytest.param(
+            'gendiff/tests/fixtures/nested/yml/file1.yml',
+            'gendiff/tests/fixtures/nested/yml/file2.yml',
+            'gendiff/tests/fixtures/nested/nested_result'
+        )
+    ]
 )
-
-json_and_yml_expected = 'gendiff/tests/fixtures/plane/first_test.txt'
-
-
-def test_flat():
-    with open(json_and_yml_expected, 'r') as file:
-        result = file.read()
-    assert json_test == result
-    assert yml_test == result
+def test_nested_stylish(test_1, test_2, expected):
+    with open(expected, 'r') as f:
+        result = f.read()
+    assert generate_diff(test_1, test_2) == result
 
 
-expected_nested = 'gendiff/tests/fixtures/nested/nested_result'
-json_nested = generate_diff(
-    'gendiff/tests/fixtures/nested/json/file1.json',
-    'gendiff/tests/fixtures/nested/json/file2.json'
+@pytest.mark.parametrize(
+    ('test_1', 'test_2', 'formatter', 'expected'),
+    [
+        pytest.param(
+            'gendiff/tests/fixtures/nested/json/file1.json',
+            'gendiff/tests/fixtures/nested/json/file2.json',
+            'plain',
+            'gendiff/tests/fixtures/nested/nested_result_plain'
+        ),
+        pytest.param(
+            'gendiff/tests/fixtures/nested/yml/file1.yml',
+            'gendiff/tests/fixtures/nested/yml/file2.yml',
+            'plain',
+            'gendiff/tests/fixtures/nested/nested_result_plain'
+        )
+    ]
 )
-yml_nested = generate_diff(
-    'gendiff/tests/fixtures/nested/yml/file1.yml',
-    'gendiff/tests/fixtures/nested/yml/file2.yml'
+def test_nested_plain(test_1, test_2, formatter, expected):
+    with open(expected, 'r') as f:
+        result = f.read()
+    assert generate_diff(test_1, test_2, formatter='plain') == result
+
+
+@pytest.mark.parametrize(
+    ('test_1', 'test_2', 'formatter', 'expected'),
+    [
+        pytest.param(
+            'gendiff/tests/fixtures/nested/json/file1.json',
+            'gendiff/tests/fixtures/nested/json/file2.json',
+            'json',
+            'gendiff/tests/fixtures/nested/nested_result_json'
+        ),
+        pytest.param(
+            'gendiff/tests/fixtures/nested/yml/file1.yml',
+            'gendiff/tests/fixtures/nested/yml/file2.yml',
+            'json',
+            'gendiff/tests/fixtures/nested/nested_result_json'
+        )
+    ]
 )
+def test_nested_json(test_1, test_2, formatter, expected):
+    with open(expected, 'r') as f:
+        result = f.read()
+    assert generate_diff(test_1, test_2, formatter='json') == result
 
 
-def test_nested():
-    with open(expected_nested, 'r') as file:
-        result = file.read()
-    assert json_nested == result
-    assert yml_nested == result
+def test_format_error():
+    with pytest.raises(Exception):
+        generate_diff('file.exe', 'file_1.exe')
 
 
-expected_nested_plain = 'gendiff/tests/fixtures/nested/nested_result_plain'
-json_nested_plain = generate_diff(
-    'gendiff/tests/fixtures/nested/json/file1.json',
-    'gendiff/tests/fixtures/nested/json/file2.json',
-    formatter='plain'
-)
-yml_nested_plain = generate_diff(
-    'gendiff/tests/fixtures/nested/yml/file1.yml',
-    'gendiff/tests/fixtures/nested/yml/file2.yml',
-    formatter='plain'
-)
-
-
-def test_nested_plain():
-    with open(expected_nested_plain, 'r') as file:
-        result = file.read()
-    assert json_nested_plain == result
-    assert yml_nested_plain == result
-
-
-expected_nested_json = 'gendiff/tests/fixtures/nested/nested_result_json'
-json_nested_json = generate_diff(
-    'gendiff/tests/fixtures/nested/json/file1.json',
-    'gendiff/tests/fixtures/nested/json/file2.json',
-    formatter='json'
-)
-yml_nested_json = generate_diff(
-    'gendiff/tests/fixtures/nested/yml/file1.yml',
-    'gendiff/tests/fixtures/nested/yml/file2.yml',
-    formatter='json'
-)
-
-
-def test_nested_json():
-    with open(expected_nested_json, 'r') as file:
-        result = file.read()
-    assert json_nested_json == result
-    assert yml_nested_json == result
+def test_formatter_error():
+    with pytest.raises(Exception):
+        generate_diff(
+            'gendiff/tests/fixtures/nested/yml/file1.yml',
+            'gendiff/tests/fixtures/nested/yml/file2.yml',
+            'error'
+        )
